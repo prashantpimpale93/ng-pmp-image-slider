03d662363e87c93ea919c52f62dc23bcb33966c8
Prashant <prashantpimpale93@gmail.com>
2019-06-09 10:21:32 +0530

    Added Angular Material

--- projects/ng2-pmp-image-slider/src/lib/imageslider/imageslider.module.ts
+++ projects/ng2-pmp-image-slider/src/lib/imageslider/imageslider.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+
 import { ImagesliderComponent } from './imageslider.component';
+
 
 @NgModule({
   declarations: [ImagesliderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports: [ImagesliderComponent]
 })
