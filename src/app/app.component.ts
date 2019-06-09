b5ecc466c2ebf36fb19e97516382b306e68fd8c1
Prashant <prashantpimpale93@gmail.com>
2019-06-09 17:00:42 +0530

    Setup local environment to run and test the library project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-pmp-image-slider';
+
+  images: any[] = [
+  'https://wallpaper21.com/wp-content/uploads/2017/08/dog-hd-cat-backgrounds-full-hd-animal-wallpaper-wpt7803807.jpg',
+  'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  'https://material.angular.io/assets/img/examples/shiba1.jpg']
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ImagesliderModule } from '../../projects/ng2-pmp-image-slider/src/public-api';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    ImagesliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
