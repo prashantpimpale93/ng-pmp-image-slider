7609463ac8a879af849e2a09c2ebcbca8d6e1f99
prashantpimpale93 <prashantpimpale93@gmail.com>
2019-06-22 16:33:35 +0530

    Added close button on pop-up window

--- projects/ng2-pmp-image-slider/src/lib/imageslider/image-preview/image-preview.component.ts
+++ projects/ng2-pmp-image-slider/src/lib/imageslider/image-preview/image-preview.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject, Optional } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'lib-image-preview',
@@ -10,11 +10,16 @@ export class ImagePreviewComponent implements OnInit {
 
   public imageUrl: string;
 
-  constructor(@Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(@Optional() @Inject(MAT_DIALOG_DATA) public data: string,
+    @Optional() public dialogRef: MatDialogRef<ImagePreviewComponent>) {
     this.imageUrl = data;
   }
 
   ngOnInit() {
+  }
+
+  close() {
+    this.dialogRef.close();
   }
 
 }
