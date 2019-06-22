29cfaede19066468aa25b64c98f9bca564311ddd
prashantpimpale93 <prashantpimpale93@gmail.com>
2019-06-22 16:53:00 +0530

    Renamed Module name in demo project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule  } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { ImagesliderModule } from '../../projects/ng2-pmp-image-slider/src/public-api';
+import { ImageSliderModule } from '../../projects/ng2-pmp-image-slider/src/public-api';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -25,7 +25,7 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatTooltipModule,
     MatSnackBarModule,
     MatDialogModule,
-    ImagesliderModule
+    ImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
